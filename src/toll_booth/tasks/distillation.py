9cb3a065650621ee17c09f197c7a15daa03de4e5
Jeff <jcubeta@algernon.solutions>
2019-08-01 10:30:41 -0400

    fussed over result packaging

--- src/toll_booth/tasks/distillation.py
+++ src/toll_booth/tasks/distillation.py
@@ -1,4 +1,8 @@
+import uuid
+from datetime import datetime
+
 import bs4
+from algernon.aws import StoredData
 
 from toll_booth.tasks.retrieve_documentation import retrieve_documentation
 
@@ -13,4 +17,13 @@ def distill_documentation(encounter_internal_id):
     id_source = documentation_data['id_source']
     documentation = documentation_data['documentation']
     distilled_text = _distill_strings(documentation)
-    return {'id_source': id_source, 'distilled_text': distilled_text}
+    extracted_data = {
+        'source': {
+            'id_source': id_source,
+            'encounter_id': documentation_data['encounter_id'],
+            'utc_generated_datetime': datetime.utcnow().isoformat(),
+            'documentation_text': distilled_text
+        }
+    }
+    stored_data = StoredData.from_object(uuid.uuid4(), extracted_data, full_unpack=True)
+    return stored_data
