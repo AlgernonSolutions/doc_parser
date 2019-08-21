93ba78ac8025e4d5c38cadcbaf78d298bc756483
Jeff <jcubeta@algernon.solutions>
2019-08-21 17:39:00 -0400

    fixed double encoding of parsed documentation

--- src/toll_booth/tasks/distillation.py
+++ src/toll_booth/tasks/distillation.py
@@ -14,23 +14,29 @@ def _distill_strings(documentation):
     return ' '.join([x for x in soup.strings])
 
 
-def _retrieve_documentation(encounter_vertex):
-    vertex_properties = encounter_vertex['vertex_properties']['stored_properties']
-    for stored_property in vertex_properties:
-        property_name = stored_property['property_name']
-        if property_name == 'documentation':
-            storage_uri = stored_property['storage_uri']
-            return s3_tasks.retrieve_s3_property(storage_uri)
-    raise RuntimeError(f'could not find documentation property on vertex: {encounter_vertex}')
+def _find_vertex_property(property_name, property_type, vertex_properties, value_name=None):
+    if not value_name:
+        value_name = 'property_value'
+    search_properties = vertex_properties[property_type]
+    for search_property in search_properties:
+        search_property_name = search_property['property_name']
+        if property_name == search_property_name:
+            return search_property[value_name]
+    raise RuntimeError(f'could not find {property_type}.{property_name} on vertex: {vertex_properties}')
+
+
+def _retrieve_documentation(vertex_properties):
+    storage_uri = _find_vertex_property('documentation', 'stored_properties', vertex_properties, 'storage_uri')
+    return s3_tasks.retrieve_s3_property(storage_uri)
 
 
 def distill_documentation(encounter_vertex):
     if isinstance(encounter_vertex, str):
         encounter_vertex = rapidjson.loads(encounter_vertex)
-    documentation_data = _retrieve_documentation(encounter_vertex)
-    id_source = documentation_data['id_source']
-    documentation = documentation_data['documentation']
-    encounter_id = documentation_data['encounter_id']
+    vertex_properties = encounter_vertex['vertex_properties']
+    documentation = _retrieve_documentation(vertex_properties)
+    id_source = _find_vertex_property('id_source', 'local_properties', vertex_properties)
+    encounter_id = int(encounter_vertex['id_value']['property_value'])
     distilled_text = _distill_strings(documentation)
     extracted_data = {
         'object_type': 'DocumentationText',
