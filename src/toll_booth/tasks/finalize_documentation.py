ca6377a235dbd44e62989b5eb94d02902300ac65
Jeff <jcubeta@algernon.solutions>
2019-08-27 11:06:09 -0400

    added task to mark post-processing complete

--- src/toll_booth/tasks/finalize_documentation.py
+++ src/toll_booth/tasks/finalize_documentation.py
@@ -5,14 +5,17 @@ import rapidjson
 from algernon import rebuild_event
 
 
-def finalize_documentation(identifier, id_value, parser_results):
+def finalize_documentation(identifier, id_value, start_time, parser_results):
     parser_results = rebuild_event(rapidjson.loads(parser_results))
     session = boto3.session.Session()
     table = session.resource('dynamodb').Table(os.environ['PROGRESS_TABLE_NAME'])
     table.update_item(
-        Key={'identifier': identifier, 'sid_value': str(id_value)},
+        Key={'identifier': identifier, 'id_value': int(id_value)},
         UpdateExpression='SET post_process = :p',
         ExpressionAttributeValues={
-            ':p': parser_results
+            ':p': {
+                'completed_at': start_time,
+                'stage_results': parser_results
+            }
         }
     )
